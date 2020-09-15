fb03c6b090b8eba0eb14239b9f6fe6100d649777
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-14 21:11:08 -0700

    Destructuring

---
+++ Destructuring.js
@@ -0,0 +1,14 @@
+// Array Destructuring 
+
+const [a , b  , c , d , e , f , g] = [2 , 3 , 4 , 5 , 6 , 7 , 8];
+console.log(a);
+console.log(b);
+console.log(g);
+
+
+//Object Destructuring
+
+const {name , age , occupation} = {name: "Dhrubo" , age: 20 , occupation:"Student"};
+console.log(name);
+console.log(occupation);
+console.log(name , age , occupation);
